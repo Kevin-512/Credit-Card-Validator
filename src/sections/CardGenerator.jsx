f4e5ae8b195330f90bab2a0a84b21ac565c510ad
Kevin-512 <108360149+Kevin-512@users.noreply.github.com>
2024-10-06 18:35:01 +0100

    Created CardGenerator template

--- src/sections/CardGenerator.jsx
+++ src/sections/CardGenerator.jsx
@@ -1,10 +1,61 @@
-const CardGenerator = () => {
-  return (
-    <section
-      id="generator"
-      className="flex flex-col container bg-red-50"
-    >Card Generator</section>
-  )
-}
+import Cards from "react-credit-cards-2";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import "react-credit-cards-2/dist/es/styles-compiled.css";
 
-export default CardGenerator
+const CardGenerator = () => {
+  const [state, setState] = useState({
+    number: "",
+    expiry: "",
+    cvc: "",
+    name: "",
+    focus: "",
+  });
+
+  const updateCardNumberWithDelay = (cardNumber) => {
+    let index = -1;
+
+    const intervalId = setInterval(() => {
+      setState((prevState) => ({
+        ...prevState,
+        number: prevState.number + cardNumber[index],
+      }));
+      index++;
+      if (index >= cardNumber.length) {
+        clearInterval(intervalId);
+      }
+    }, 300);
+  };
+
+  const handleStartTyping = () => {
+    const cardNumber = "1234 5678 9012 3456";
+    setState((prevState) => ({ ...prevState, number: "" }));
+    updateCardNumberWithDelay(cardNumber);
+  };
+
+  
+
+  return (
+    <section id="generator" className="flex flex-col container gap-10">
+      <h1 className="text-center text-8xl font-bold text-amber-600 underline">
+        <span className="z-10">Card Number Generator</span>
+      </h1>
+      <div className="scale-110 transform">
+        <Cards
+          cvc={state.cvc}
+          expiry={state.expiry}
+          focused={state.focus}
+          name={state.name}
+          number={state.number}
+        />
+      </div>
+      <div className="flex justify-center items-center">
+        <Button className="text-2xl" onClick={handleStartTyping}>
+          Generate
+        </Button>
+      </div>
+    </section>
+  );
+};
+
+export default CardGenerator;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontSize: {
-      xs: ["12px", "16px"],
-      sm: ["14px", "20px"],
-      base: ["16px", "19.5px"],
-      lg: ["18px", "21.94px"],
-      xl: ["20px", "24.38px"],
-      "2xl": ["24px", "29.26px"],
-      "3xl": ["28px", "50px"],
-      "4xl": ["48px", "58px"],
-      "8xl": ["96px", "106px"],
-    },
-    container: {
-      center: true,
-      padding: "15px",
-      screens: {
-        sm: "640px",
-        md: "768px",
-        lg: "960px",
-        xl: "1200px",
-      },
-    },
-    extend: {
-      fontFamily: {
-        sans: ['Oswald', 'sans-serif'],
-      },
-    },
+  	fontSize: {
+  		xs: ["12px", "16px"],
+  		sm: ["14px", "20px"],
+  		base: ["16px", "19.5px"],
+  		lg: ["18px", "21.94px"],
+  		xl: ["20px", "24.38px"],
+  		'2xl': ["24px", "29.26px"],
+  		'3xl': ["28px", "50px"],
+  		'4xl': ["48px", "58px"],
+  		'8xl': ["96px", "106px"]
+  	},
+  	container: {
+  		center: 'true',
+  		padding: '15px',
+  		screens: {
+  			sm: '640px',
+  			md: '768px',
+  			lg: '960px',
+  			xl: '1200px'
+  		}
+  	},
+  	extend: {
+  		fontFamily: {
+  			sans: ['Oswald', 'sans-serif']
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
